933c848704665f2cf64dc94d5e7294880974ee0e
Ethem Sahbaz <ethemsahbaz@outlook.com>
2025-04-23 21:49:56 +0200

    Renamed method

--- src/AtomicFlow.Application/Authentication/IIdentityProvider.cs
+++ src/AtomicFlow.Application/Authentication/IIdentityProvider.cs
@@ -1,4 +1,5 @@
 ﻿namespace AtomicFlow.Application.Authentication;
 internal interface IIdentityProvider
 {
+    Task<string> RegisterUserAsync(string userName, string email, string password);
 }
--- src/AtomicFlow.Application/Features/Users/RegisterUserCommandService.cs
+++ src/AtomicFlow.Application/Features/Users/RegisterUserCommandService.cs
@@ -1,13 +1,22 @@
-﻿using AtomicFlow.Domain.Users;
+﻿using AtomicFlow.Application.Authentication;
+using AtomicFlow.Domain.Users;
 using AtomicFlow.SharedKernel;
 
 namespace AtomicFlow.Application.Features.Users;
 internal sealed class RegisterUserCommandService
 {
     private readonly IUserRepository _userRepository;
+    private readonly IIdentityProvider _identityProvider;
 
+    public RegisterUserCommandService(
+        IUserRepository userRepository,
+        IIdentityProvider identityProvider)
+    {
+        _userRepository = userRepository;
+        _identityProvider = identityProvider;
+    }
 
-    public async Task<Result<User>> Register(RegisterUserRequest request)
+    public async Task<Result<User>> RegisterAsync(RegisterUserRequest request)
     {
         bool emailExists = await _userRepository.EmailExists(request.Email);
 
@@ -15,10 +24,6 @@ internal sealed class RegisterUserCommandService
         {
             return Result.Failure<User>(UserErrors.EmailAlreadyExists);
         }
-
-
-
-
 
         return null;
     }
