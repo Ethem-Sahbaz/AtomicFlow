7cfe205d3d37f87af29d8d08a06278379c447929
Ethem-Sahbaz <ethemsahbaz@outlook.com>
2025-04-27 09:59:23 +0200

    Added blazor frontend project

--- src/AtomicFlow.Api/Program.cs
+++ src/AtomicFlow.Api/Program.cs
@@ -2,6 +2,11 @@ using AtomicFlow.Application;
 using AtomicFlow.Application.Features.Users.RegisterUser;
 using AtomicFlow.Infrastructure;
 
+// 1. Setup Azure B2C for Blazor WASM Application
+// 2. Configure Api authentication to accept azure b2c jwt
+// 3. Setup a database
+// 4. Add new users to database
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
