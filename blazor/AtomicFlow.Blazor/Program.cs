d2834c755015e77e4506cd2d5633c5a316a86163
Ethem-Sahbaz <ethemsahbaz@outlook.com>
2025-05-05 18:57:07 +0200

    Added access token to auth

--- blazor/AtomicFlow.Blazor/Program.cs
+++ blazor/AtomicFlow.Blazor/Program.cs
@@ -17,12 +17,11 @@ builder.Services.AddAuth0WebAppAuthentication(options =>
     options.Domain = builder.Configuration["Auth0:Domain"]!;
     options.ClientId = builder.Configuration["Auth0:ClientId"]!;
     options.ClientSecret = builder.Configuration["Auth0:ClientSecret"]!;
+})
+.WithAccessToken(accessTokenOptions =>
+{
+    accessTokenOptions.Audience = builder.Configuration["Auth0:Audience"]!;
 });
-
-//     .WithAccessToken(accessTokenOptions =>
-// {
-//     accessTokenOptions.Audience = builder.Configuration["Auth0:Audience"]!;
-// });
 
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddHttpForwarder();
